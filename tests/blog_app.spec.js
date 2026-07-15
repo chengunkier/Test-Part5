25fbbdd344d902101f1e06e4c4337c1c9058e9ff
chengunkier <inabamicheal97@gmail.com>
2026-07-15 10:28:38 +0300

    completed exercise 5.18: Blog List End To End Testing, step 2

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -2,7 +2,10 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
+    // empty the database
     await request.post('http://localhost:3003/api/testing/reset')
+
+    // create a user for the backend
     await request.post('http://localhost:3003/api/users', {
       data: {
         name: 'Matti Luukkainen',
@@ -18,5 +21,24 @@ describe('Blog app', () => {
     await expect(page.getByText('Log in to application')).toBeVisible()
     await expect(page.getByRole('textbox').first()).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('wrongpassword')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
   })
 })
